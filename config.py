036e0ae0de908cd7857772f1620206c4034bd6af
cwiggan <info@cwiggan.com>
2017-08-14 18:22:16 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+# Enable Flask's debugging features. Should be False in production
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'postgresql://sekobsl@localhost/guestbook'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ run.py
@@ -0,0 +1,6 @@
+# run.py run app
+
+from app import app
+
+if __name__ == '__main__':
+    app.run()
